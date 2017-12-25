27b815378c1c192cdfc176cbded2377d45367738
Cheng <cheng@strikingly.com>
2017-12-25 15:40:39 +0800

    evolve(demo1):dependency injection - constructor injection

--- spring-demo-1/src/com/luv2code/springdemo/HelloSpringApp.java
+++ spring-demo-1/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -14,6 +14,7 @@ public class HelloSpringApp {
 
 		// call methods on bean
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
 
 		// close the context
 		context.close();
