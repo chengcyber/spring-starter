f1199e21dc38bad09988ced7b1ec2b1ab13cc605
Cheng <kimochg@outlook.com>
2017-12-25 15:54:47 +0800

    evolve(demo1):dependency injection - literal value injection

--- spring-demo-1/src/com/luv2code/springdemo/CricketCoach.java
+++ spring-demo-1/src/com/luv2code/springdemo/CricketCoach.java
@@ -15,6 +15,24 @@ public class CricketCoach implements Coach {
 		this.fortuneService = fortuneService;
 	}
 
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside our setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside our setter method - setTeam");
+		this.team = team;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Practice fast bowling for 15minutes";
--- spring-demo-1/src/com/luv2code/springdemo/SetterDemoApp.java
+++ spring-demo-1/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -15,6 +15,8 @@ public class SetterDemoApp {
 		// call methods on bean object
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getTeam());
+		System.out.println(theCoach.getEmailAddress());
 
 		// close the context nicely
 		ctx.close();
