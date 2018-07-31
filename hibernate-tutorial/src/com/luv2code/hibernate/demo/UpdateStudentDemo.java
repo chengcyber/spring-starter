51679c05b6fc2c58c89c9524ccb2ea4c1efd4daf
Cheng <kimochg@outlook.com>
2018-07-31 14:11:12 +0800

    evolve(hibernate): Update Object via Hibernate

---
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,58 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			int studentId = 1;
+			
+			// now get a new session and start transaction
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			// retrieve student based on the id: primary key
+			System.out.println("Getting student with Id " + studentId);
+			
+			Student myStudent = session.get(Student.class, studentId);
+			
+			System.out.println("Updating student...");
+			myStudent.setFirstName("Scooby");
+			
+			System.out.println("Get complete: " + myStudent);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			// update email for all students
+			session = factory.getCurrentSession();
+			session.beginTransaction();
+			
+			session.createQuery("update Student set email='foo@gmail.com'")
+					.executeUpdate();
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		} finally {
+			factory.close();
+		}
+	}
+
+}
