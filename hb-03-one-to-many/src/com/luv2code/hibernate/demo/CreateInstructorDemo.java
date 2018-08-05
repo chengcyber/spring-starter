1ba0eb6c4071b37c5f80544ca674b3fb6f9fe59f
Cheng <kimochg@outlook.com>
2018-08-05 12:02:32 +0800

    evolve(hibernate): @OneToMany create instructor demo

---
+++ hb-03-one-to-many/src/com/luv2code/hibernate/demo/CreateInstructorDemo.java
@@ -0,0 +1,58 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateInstructorDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Instructor.class)
+                                    .addAnnotatedClass(InstructorDetail.class)
+                                    .addAnnotatedClass(Course.class)
+                                    .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // create objects
+            Instructor instructor =
+                    new Instructor("Susan", "Public", "susan@gmail.com");
+            InstructorDetail instructorDetail =
+                    new InstructorDetail("http://you.tb", "Video Game");
+
+            // associate the objects
+            instructor.setInstructorDetail(instructorDetail);
+
+            // start transaction
+            session.beginTransaction();
+
+            // save the instructor
+            System.out.println("Saving instructor: " + instructor);
+            session.save(instructor);
+
+            // commit
+            session.getTransaction().commit();
+
+            System.out.println("DONE!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            // add clean up code
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
