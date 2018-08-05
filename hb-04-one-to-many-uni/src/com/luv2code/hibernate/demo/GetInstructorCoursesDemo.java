a02773e48326857149b70775d0f18973eea3e989
Cheng <kimochg@outlook.com>
2018-08-05 17:18:51 +0800

    evolve(hibernate): @OneToMany unidirectional setup

---
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,54 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorCoursesDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Instructor.class)
+                                    .addAnnotatedClass(InstructorDetail.class)
+                                    .addAnnotatedClass(Course.class)
+                                    .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start transaction
+            session.beginTransaction();
+
+            // get the instructor from db
+            int id = 1;
+            Instructor instructor = session.get(Instructor.class, id);
+
+            System.out.println("Instructor: " + instructor);
+
+            // get courses for instructor
+            System.out.println("Courses: " + instructor.getCourses());
+
+            // commit
+            session.getTransaction().commit();
+
+            System.out.println("DONE!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            // add clean up code
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
---
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/entity/Course.java
@@ -0,0 +1,67 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="course")
+public class Course {
+
+    // define our fields
+    // define constructors
+    // define getter setters
+    // define toString
+    // annotate fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int Id;
+
+    @Column(name="title")
+    private String title;
+
+    @ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+    @JoinColumn(name="instructor_id")
+    private Instructor instructor;
+
+    public Course() {
+
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", instructor=" + instructor +
+                '}';
+    }
+}
