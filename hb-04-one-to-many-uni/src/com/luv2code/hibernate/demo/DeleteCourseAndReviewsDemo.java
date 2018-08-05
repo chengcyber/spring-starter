064ea60091cb48d2b5a0c92a4cd0dab77cf24d76
Cheng <kimochg@outlook.com>
2018-08-05 17:58:36 +0800

    evolve(hibernate): @OneToMany unidirectional delete demo

---
+++ hb-04-one-to-many-uni/src/com/luv2code/hibernate/demo/DeleteCourseAndReviewsDemo.java
@@ -0,0 +1,60 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteCourseAndReviewsDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Instructor.class)
+                                    .addAnnotatedClass(InstructorDetail.class)
+                                    .addAnnotatedClass(Course.class)
+                                    .addAnnotatedClass(Review.class)
+                                    .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start transaction
+            session.beginTransaction();
+
+            // get course
+            int id = 10;
+            Course course = session.get(Course.class, id);
+
+            // print the course
+            System.out.println(course);
+
+            // print course reviews
+            System.out.println(course.getReviews());
+
+            // delete the course
+            session.delete(course);
+
+            // commit
+            session.getTransaction().commit();
+
+            System.out.println("DONE!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+            // add clean up code
+            session.close();
+            factory.close();
+        }
+
+    }
+
+}
